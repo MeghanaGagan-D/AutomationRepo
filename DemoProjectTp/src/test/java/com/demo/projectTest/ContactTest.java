ca6a9d5484b93acc4acb396688969bf8b528c6d3
meghana <meghanatdgowda@gmail.com>
2024-07-30 17:49:32 +0530

    Initial commit

---
+++ DemoProjectTp/src/test/java/com/demo/projectTest/ContactTest.java
@@ -0,0 +1,11 @@
+package com.demo.projectTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+@Test
+public void createContactTest()
+{
+	System.out.println("========contact created=========");
+}
+}
