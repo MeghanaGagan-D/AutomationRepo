6d765f7d45fa3fecfcd472632f918dd8de3f5e19
meghana <meghanatdgowda@gmail.com>
2024-07-30 19:00:11 +0530

    second commit

--- DemoProjectTp/src/test/java/com/demo/projectTest/ContactTest.java
+++ DemoProjectTp/src/test/java/com/demo/projectTest/ContactTest.java
@@ -6,6 +6,10 @@ public class ContactTest {
 @Test
 public void createContactTest()
 {
+	String Environment=System.getenv("url");
+	String Browser=System.getenv("browser");
+	System.out.println(Environment);
+	System.out.println(Browser);
 	System.out.println("========contact created=========");
 }
 }
